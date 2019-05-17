7e180e622c96cdf523e2439557f314cde68e5721
rjsmall90 <rjsmall90@gmail.com>
2019-05-17 13:35:27 -0400

    native app running

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FoodReyes-View
+
+collaborating with a partner to build a touch screen inventory and customer facing application for food merchants           
---
+++ components/login/login.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Input Box for Admin Username</Text>
+                <Text>Input Box for Admin Password</Text>
+            </View>
+        );
+    }
+}
+
+export default Login; 
